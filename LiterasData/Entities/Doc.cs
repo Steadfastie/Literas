7da2a0d30bac37a82a7ca303accc4b3362cc11c6
lkumbrella <L.Kumbrella@outlook.com>
2023-04-11 15:18:33 +0300

    Deltas API migration + validation

--- LiterasData/Entities/Doc.cs
+++ LiterasData/Entities/Doc.cs
@@ -1,9 +1,16 @@
-﻿namespace LiterasData.Entities;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json;
+
+namespace LiterasData.Entities;
 
 public class Doc
 {
     public Guid Id { get; set; }
     public Guid CreatorId { get; set; }
     public string Title { get; set; }
+    public JsonDocument? TitleDelta { get; set; }
+
     public string Content { get; set; }
+    public JsonDocument? ContentDeltas { get; set; }
 }
--- LiterasData/Migrations/20230410195559_AddJsonDelta.Designer.cs
+++ LiterasData/Migrations/20230410195559_AddJsonDelta.Designer.cs
@@ -4,6 +4,7 @@ using System.Text.Json;
 using LiterasData;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace LiterasData.Migrations
 {
     [DbContext(typeof(NotesDBContext))]
-    partial class NotesDBContextModelSnapshot : ModelSnapshot
+    [Migration("20230410195559_AddJsonDelta")]
+    partial class AddJsonDelta
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
