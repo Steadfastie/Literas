727a0f2a4f1203e7898f633593baf8812a566ca3
lkumbrella <L.Kumbrella@outlook.com>
2023-05-08 22:20:48 +0300

    login page layout and guard

---
+++ Angular/Literas/src/app/guards/unauth.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import { Observable } from 'rxjs';
+import {AuthService} from "../services/auth/auth.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UnauthGuard implements CanActivate {
+  constructor(private authService: AuthService,
+              private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    this.authService.getToken().then(token => {
+      if (token === null){
+        return true;
+      }
+      this.router.navigate(['/']);
+      return false;
+    });
+    return false;
+  }
+
+}
